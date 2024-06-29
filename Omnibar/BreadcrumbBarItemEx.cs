812e3f544c0da8d47e58b3edd0316cd283da5e2e
0x5bfa <62196528+0x5bfa@users.noreply.github.com>
2024-06-30 05:03:45 +0900

    Add project files.

---
+++ Omnibar/BreadcrumbBarItemEx.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omnibar
+{
+	internal class BreadcrumbBarItemEx : BreadcrumbBarItem
+	{
+		public BreadcrumbBarItemEx()
+		{
+			DefaultStyleKey = typeof(BreadcrumbBarItemEx);
+		}
+
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			if (GetTemplateChild("PART_ChevronButton") is Button button)
+			{
+				button.RegisterPropertyChangedCallback(ButtonBase.IsPressedProperty, (sender, args) =>
+				{
+					VisualStateManager.GoToState(button, "Pressed", true);
+					VisualStateManager.GoToState(button, button.IsPressed ? "Expanded" : "NotExpanded", true);
+				});
+
+				button.RegisterPropertyChangedCallback(ButtonBase.IsPointerOverProperty, (sender, args) =>
+				{
+					VisualStateManager.GoToState(button, "PointerOver", true);
+				});
+			}
+		}
+	}
+}
